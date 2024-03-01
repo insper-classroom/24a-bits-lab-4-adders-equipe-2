95c3997d656b3daab943146f0da660702ee1abbb
Joao-Pedro-Queiroz <142129883+Joao-Pedro-Queiroz@users.noreply.github.com>
2024-03-01 20:34:15 +0000

    adder2bits feito
    Co-authored-by: carloshernani-CH <carloshernani-CH@users.noreply.github.com>

--- ula_modules.py
+++ ula_modules.py
@@ -1,33 +1,38 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-
-"""
-
 from myhdl import *
-
 
 @block
 def halfAdder(a, b, soma, carry):
     @always_comb
     def comb():
-        pass
-
+        soma.next = a ^ b
+        carry.next = a & b  
     return instances()
 
-
 @block
-def fullAdder(a, b, c, soma, carry):
+def fullAdder(a, b, c, soma, carry_out):
+    s = [Signal(bool(0)) for _ in range(3)]
+    carry = [Signal(bool(0)) for _ in range(2)]
+
+    ha1 = halfAdder(a, b, s[0], carry[0])
+    ha2 = halfAdder(s[0], c, s[1], carry[1])
+    
     @always_comb
     def comb():
-        pass
-
+        s[2].next = s[1]
+        carry_out.next = carry[0] | carry[1]
+        soma.next = s[2]
     return instances()
-
 
 @block
-def adder2bits(x, y, soma, carry):
+def adder2bits(x, y, soma, vaiUm):
+    
+    c0 = Signal(bool(0)) 
+    HA = halfAdder(x[0], y[0], soma[0], c0)
+    FA = fullAdder(x[1], y[1], c0, soma[1], vaiUm)
+
+    
     return instances()
+
 
 
 @block
